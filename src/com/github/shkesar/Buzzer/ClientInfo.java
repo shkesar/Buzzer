844ec47204b34afb6a4fa8c2a84951283b1d8530
Shubham Keserwani <shubhamkesar@live.com>
2014-02-01 08:21:33 +0530

    Defined the ClientInfo class

--- src/com/github/shkesar/Buzzer/ClientInfo.java
+++ src/com/github/shkesar/Buzzer/ClientInfo.java
@@ -1,5 +1,31 @@
 package com.github.shkesar.Buzzer;
 
 public class ClientInfo {
+    private String name;
+    private int score = 0;
 
+    public ClientInfo(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public ClientInfo(String name) {
+        this(name, 0);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void addScore(int addedScore) {
+        this.score += addedScore;
+    }
 }
